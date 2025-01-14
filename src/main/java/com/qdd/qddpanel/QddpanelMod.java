c2c39a329173319dca0cf6a49f90f79ffaf76a24
caizheng.qin <dhdxqcz@gmail.com>
2025-01-14 19:02:21 +0800

    小修改1

--- src/main/java/com/qdd/qddpanel/QddpanelMod.java
+++ src/main/java/com/qdd/qddpanel/QddpanelMod.java
@@ -1,6 +1,5 @@
 package com.qdd.qddpanel;
 
-import com.qdd.qddpanel.CommonProxy;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.Mod.Instance;
@@ -8,7 +7,8 @@ import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-
+import net.minecraftforge.fml.common.network.NetworkRegistry;
+import net.minecraftforge.fml.common.network.simpleimpl.SimpleNetworkWrapper;
 
 
 @Mod(modid = QddpanelMod.MODID, name = QddpanelMod.NAME, version = QddpanelMod.VERSION, dependencies = "required-after:narutomod")
@@ -17,7 +17,7 @@ public class QddpanelMod {
     public static final String MODID = "qddpanel";
     public static final String NAME = "QDDPanel Mod";
     public static final String VERSION = "1.0.0";
-    
+    public static final SimpleNetworkWrapper PACKET_HANDLER = NetworkRegistry.INSTANCE.newSimpleChannel("qddpanel:a");
     @SidedProxy(clientSide = "com.qdd.qddpanel.ClientProxy", 
             serverSide = "com.qdd.qddpanel.CommonProxy")
     public static CommonProxy proxy;
